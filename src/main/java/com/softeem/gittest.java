1bc7a7b662c5d3195ddf0ceb941aee891068bc6a
fqy <fqy@qq.com>
2021-11-27 17:36:41 +0800

    first commit

---
+++ src/main/java/com/softeem/gittest.java
@@ -0,0 +1,11 @@
+package com.softeem;
+
+/**
+ * @author fqy
+ * 2021/11/27
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("heee");
+    }
+}
---
+++ src/main/java/com/softeem/gittest2.java
@@ -0,0 +1,11 @@
+package com.softeem;
+
+/**
+ * @author fqy
+ * 2021/11/27
+ */
+public class gittest2 {
+    public static void main(String[] args) {
+        System.out.println("heee");
+    }
+}
