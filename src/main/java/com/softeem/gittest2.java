6621fd8e52e87b00114498aadee9c9dba7120a56
fqy15972679822 <87740606+fqy15972679822@users.noreply.github.com>
2021-11-27 20:33:38 +0800

    Update gittest2.java

--- src/main/java/com/softeem/gittest2.java
+++ src/main/java/com/softeem/gittest2.java
@@ -6,12 +6,6 @@ package com.softeem;
  */
 public class gittest2 {
     public static void main(String[] args) {
-        System.out.println("heee");
-        System.out.println("heee");
-        System.out.println("heee");
-        System.out.println("3333");
-        System.out.println("444");
-        System.out.println("555");
         System.out.println("tijiao");
     }
 }
