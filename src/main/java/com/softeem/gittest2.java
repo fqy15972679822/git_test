b4eb10601d9667fc3be6c9f61c309eba4fa9a74e
fqy <fqy@qq.com>
2021-11-27 20:12:47 +0800

    third3 commit

--- src/main/java/com/softeem/gittest2.java
+++ src/main/java/com/softeem/gittest2.java
@@ -8,6 +8,8 @@ public class gittest2 {
     public static void main(String[] args) {
         System.out.println("heee");
         System.out.println("3333");
+        System.out.println("3333");
+        System.out.println("master");
         System.out.println("444");
         System.out.println("555");
     }
