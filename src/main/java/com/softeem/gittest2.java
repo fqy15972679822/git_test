608f5a8e6826b6ba379e4460e153df680105658c
fqy <fqy@qq.com>
2021-11-27 20:13:50 +0800

    third3 commit

--- src/main/java/com/softeem/gittest2.java
+++ src/main/java/com/softeem/gittest2.java
@@ -6,7 +6,7 @@ package com.softeem;
  */
 public class gittest2 {
     public static void main(String[] args) {
-        System.out.println("heee");
+        System.out.println("123123");
         System.out.println("heee");
         System.out.println("heee");
         System.out.println("3333");
